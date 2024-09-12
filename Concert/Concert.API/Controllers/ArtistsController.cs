f37124484187a39412afca7bb507662153b1428d
Alberto <alberto.izquierdomo@gmail.com>
2024-09-12 19:41:58 +0200

    Create Artists endpoint and create get all method

---
+++ Concert/Concert.API/Controllers/ArtistsController.cs
@@ -0,0 +1,30 @@
+﻿using Concert.API.Data;
+using Concert.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concert.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly ConcertDbContext _concertDbContext;
+
+        public ArtistsController(ConcertDbContext concertDbContext)
+        {
+            _concertDbContext = concertDbContext;
+        }
+
+        // Get all artists
+        // GET: https://localhost:portnumber/api/artists
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var artists = _concertDbContext.Artists.ToList();
+
+            return Ok(artists);
+        }
+    }
+}
