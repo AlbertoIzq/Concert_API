dd06f1137b2255014c25dbf823c7ce4a61910239
Alberto <alberto.izquierdomo@gmail.com>
2024-09-12 18:01:55 +0200

    Add DbContext and inject it

---
+++ Concert/Concert.API/Data/ConcertDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Concert.API.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concert.API.Data
+{
+    public class ConcertDbContext : DbContext
+    {
+        public ConcertDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
--- Concert/Concert.API/Program.cs
+++ Concert/Concert.API/Program.cs
@@ -1,3 +1,6 @@
+using Concert.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ConcertDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConcertConnectionString")));
 
 var app = builder.Build();
 
