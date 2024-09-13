e5ac874463ae313f386d3adf4d0f1d4cfaff616c
Alberto <alberto.izquierdomo@gmail.com>
2024-09-13 12:30:02 +0200

    Create Artist DTO and include in Controller

--- Concert/Concert.API/Controllers/ArtistsController.cs
+++ Concert/Concert.API/Controllers/ArtistsController.cs
@@ -1,5 +1,6 @@
 ﻿using Concert.API.Data;
 using Concert.API.Models.Domain;
+using Concert.API.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,9 +23,23 @@ namespace Concert.API.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var artists = _concertDbContext.Artists.ToList();
+            // Get data from database - Domain Model
+            var artistsDomain = _concertDbContext.Artists.ToList();
 
-            return Ok(artists);
+            // Map Domain Model to DTO
+            var artistsDto = new List<ArtistDto>();
+            foreach (var artistDomain in artistsDomain)
+            {
+                artistsDto.Add(new ArtistDto()
+                {
+                    Id = artistDomain.Id,
+                    Name = artistDomain.Name,
+                    ArtistImageUrl = artistDomain.ArtistImageUrl
+                });
+            }
+
+            // Return DTO back to client
+            return Ok(artistsDto);
         }
 
         // GET SINGLE ARTIST (Get Artist by Id)
@@ -33,17 +48,24 @@ namespace Concert.API.Controllers
         [Route("{id:Guid}")]
         public IActionResult GetById([FromRoute] Guid id)
         {
-            // Find() only takes primary key as a parameter
-            //var artist = _concertDbContext.Artists.Find(id);
+            // Get data from database - Domain Model
+            var artistDomain = _concertDbContext.Artists.FirstOrDefault(x => x.Id == id);
 
-            var artist = _concertDbContext.Artists.FirstOrDefault(x => x.Id == id);
-
-            if (artist == null)
+            if (artistDomain == null)
             {
                 return NotFound();
             }
 
-            return Ok(artist);
+            // Convert Domain Model to DTO
+            var artistDto = new ArtistDto()
+            {
+                Id = artistDomain.Id,
+                Name = artistDomain.Name,
+                ArtistImageUrl = artistDomain.ArtistImageUrl
+            };
+
+            // Return DTO back to client
+            return Ok(artistDto);
         }
     }
 }
