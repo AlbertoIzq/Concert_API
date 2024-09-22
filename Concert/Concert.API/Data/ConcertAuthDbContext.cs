8600a77f0481286814fee1d9945244e1edf2d718
Alberto <alberto.izquierdomo@gmail.com>
2024-09-22 16:39:09 +0200

    Seed roles tables, add migration and update db

--- Concert/Concert.API/Data/ConcertAuthDbContext.cs
+++ Concert/Concert.API/Data/ConcertAuthDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Concert.API.Models.Domain;
+using Concert.Utility;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +11,32 @@ namespace Concert.API.Data
     {
         public ConcertAuthDbContext(DbContextOptions<ConcertAuthDbContext> dbContextOptions) : base(dbContextOptions)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole
+                {
+                    Id = SD.READER_ROLE_ID,
+                    ConcurrencyStamp = SD.READER_ROLE_ID,
+                    Name = SD.READER_ROLE_NAME,
+                    NormalizedName = SD.READER_ROLE_NAME.ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = SD.WRITER_ROLE_ID,
+                    ConcurrencyStamp = SD.WRITER_ROLE_ID,
+                    Name = SD.WRITER_ROLE_NAME,
+                    NormalizedName = SD.WRITER_ROLE_NAME.ToUpper()
+                }
+            };
+
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
