03ebe114365a1ea28f7cc58d451e6908a0f2721d
Alberto <alberto.izquierdomo@gmail.com>
2024-09-22 17:21:27 +0200

    Create Auth controller and Register method

---
+++ Concert/Concert.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Concert.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Concert.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // POST /api/auth/register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser()
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await _userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                // Add roles to this user
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await _userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered succesfully! Please login");
+                    }
+                }
+            }
+
+            return BadRequest("Something went wrong");
+        }
+    }
+}
---
+++ Concert/Concert.API/Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using Concert.API.Models.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Concert.API.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
