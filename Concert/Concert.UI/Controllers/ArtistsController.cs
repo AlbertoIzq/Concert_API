2fb77e3cef5972b9981b32c69d19f163e08ce4a2
Alberto <alberto.izquierdomo@gmail.com>
2024-09-26 14:11:58 +0200

    Consume Artists GetAll from UI project

---
+++ Concert/Concert.UI/Controllers/ArtistsController.cs
@@ -0,0 +1,38 @@
+﻿using Concert.UI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Concert.UI.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ArtistsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ArtistDto> response = new List<ArtistDto>();
+
+            try
+            {
+                // Get all regions from web API
+                var client = _httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7033/api/artists");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<ArtistDto>>()); 
+            }
+            catch (Exception)
+            {
+                // Log the exception
+                throw;
+            }
+
+            return View(response);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -27,15 +27,18 @@ _CONCEPTS USED_
 
 _TODO_
 
-- Create an IRepository<T> and refactor all repositories?
-- Correct navigation properties showing null value when Song Update
-- Add more filtering and sorting for Song
-- Add filtering and sorting for Artist
-- Add pagination for Artist
-- Add Genre, Language and Service Controllers
-- Add logging to all action methods
-- Change logging messages by using const strings from Utility project. Add reflection to get method name and class name to add it into logging message.
-- Add API versioning?
+- API
+  - Create an IRepository<T> and refactor all repositories?
+  - Correct navigation properties showing null value when Song Update
+  - Add more filtering and sorting for Song
+  - Add filtering and sorting for Artist
+  - Add pagination for Artist
+  - Add Genre, Language and Service Controllers
+  - Add logging to all action methods
+  - Change logging messages by using const strings from Utility project. Add reflection to get method name and class name to add it into logging message.
+  - Add API versioning?
+- UI
+  - Refactor base URL to appsettings
 
 _BUGS_
 
